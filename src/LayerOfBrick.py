5082dc9c8f575f5dca270c09ff0f9cb41b727a1a
NamDo8467 <huynhnamdo@gmail.com>
2022-09-26 14:33:37 -0400

    detect collision without matching color

--- src/LayerOfBrick.py
+++ src/LayerOfBrick.py
@@ -1,9 +1,23 @@
+from src.Brick import Brick
+
+
 class LayerOfBrick:
     def __init__(self):
+        self.__color_list = None
         self.layer = {}
 
-    def getAllBrick(self):
+    def addBrick(self, brick):
+        self.layer[f'{brick.x}'] = brick
+
+    def getAllIndexAndBricks(self):
         return self.layer.items()
+
+    def getAllBricks(self):
+        return self.layer.values()
 
     def getBrickByIndex(self, index):
         return self.layer[f'{index}']
+
+    def removeBrick(self, x, y):
+        empty_brick = None
+        self.layer[f'{x}'] = empty_brick
--- src/Wall.py
+++ src/Wall.py
@@ -2,6 +2,7 @@ from collections import deque
 from src.Brick import Brick
 from src.Colors import colors
 from random import randrange
+from src.LayerOfBrick import LayerOfBrick
 
 
 class Wall:
@@ -11,17 +12,23 @@ class Wall:
 
     def __init__(self):
         self.queue = deque()
+        self.firstLayer = LayerOfBrick()
         for i in range(0, 440, 20):  # range(start=0, stop=440 (not included), step=20)
             brick = Brick(i, 0, self.__color_list[randrange(0, self.__number_of_colors - 1)])
-            self.queue.append(brick)
+            self.firstLayer.addBrick(brick)
+        self.queue.append(self.firstLayer)
 
     def addLayerOfBricks(self):
-        for brick in self.queue:
-            if brick.y == 410 or brick.y > 410:  # 410 is the Y-coordinate that hits the spaceship which will make the game stops
-                return
-            brick.y += 1
+        for layer in self.queue:
+            for brick in layer.getAllBricks():
+                if brick is not None:
+                    if brick.y == 410 or brick.y > 410:  # 410 is the Y-coordinate that hits the spaceship which will make the game stops
+                        return
+                    brick.y += 1
 
+        new_layer = LayerOfBrick()
         for i in range(0, 440, 20):  # range(start=0, stop=440 (not included), step=20)
             brick = Brick(i, self.__distance_between_layer, self.__color_list[randrange(0, self.__number_of_colors - 1)])
-            self.queue.append(brick)
+            new_layer.addBrick(brick)
+        self.queue.append(new_layer)
         self.__distance_between_layer -= 10
